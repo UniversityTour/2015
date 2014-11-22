77d11a68e1544bf9c70b7a6cc76ffc71dca30425
Dominic Giebert <giebert@in.tum.de>
2014-11-22 20:15:16 +0100

    Collision Asteroids

--- Unity/Assets/Scripts/AsteroidSpawner.cs
+++ Unity/Assets/Scripts/AsteroidSpawner.cs
@@ -8,6 +8,8 @@ public class AsteroidSpawner : MonoBehaviour {
 	public GameObject[] Asteroids;
 	public float spawnValue = 1f;
 	public float maxAsteroidSpeed = 3f;
+	public int maxAsteroids = 10;
+	public int numAsteroids = 0;
 	// Use this for initialization
 	private float timePassed = 0.0f;
 
@@ -22,15 +24,16 @@ public class AsteroidSpawner : MonoBehaviour {
 
 	// Range [-8.25,8.25]
 	void spawnAsteroids(){
-		float probability = timePassed / UnityEngine.Random.value  ;
-		if(probability > spawnValue){
+		float probability = timePassed / UnityEngine.Random.value;
+		if(probability > spawnValue && numAsteroids < maxAsteroids){
+			numAsteroids++;
 			GameObject newAsteroid = Instantiate(Asteroids[(int)(UnityEngine.Random.value * Asteroids.Length)]) as GameObject;
-			newAsteroid.GetComponent<AsteroidScript>().Speed = UnityEngine.Random.value * maxAsteroidSpeed;
+			newAsteroid.transform.GetChild(0).GetComponent<AsteroidScript>().Speed = UnityEngine.Random.value * maxAsteroidSpeed;
 			float xPos = UnityEngine.Random.value * 16.5f;
 			Vector3 pos = newAsteroid.transform.position;
 			pos.x = xPos > 8.25f ? -(xPos - 8.25f) : xPos;
 			pos.z = -5f;
-			newAsteroid.transform.position = pos;
+			newAsteroid.transform.GetChild(0).transform.position = pos;
 			timePassed = 0f;
 		}
 	}
