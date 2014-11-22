c751104b4b7ca4fcad8260edfd7fe7ac5ccec291
Dominic Giebert <giebert@in.tum.de>
2014-11-22 16:17:56 +0100

    Spaceship movement + firing

--- Unity/Assets/Scripts/AsteroidSpawner.cs
+++ Unity/Assets/Scripts/AsteroidSpawner.cs
@@ -7,13 +7,13 @@ public class AsteroidSpawner : MonoBehaviour {
 
 	public GameObject[] Asteroids;
 	public float spawnValue = 1f;
+	public float maxAsteroidSpeed = 3f;
 	// Use this for initialization
 	private List<GameObject> liveAsteroids;
 	private float timePassed = 0.0f;
 
 	void Start () {
 		liveAsteroids = new List<GameObject>();
-
 		UnityEngine.Random.seed = (int)(new DateTime()).Ticks; 
 	}
 	// Update is called once per frame
@@ -35,18 +35,18 @@ public class AsteroidSpawner : MonoBehaviour {
 			liveAsteroids.RemoveAt(index);
 			Destroy(del);
 		}
-		
 	}
-	
+
 	// Range [-8.25,8.25]
 	void spawnAsteroids(){
 		float probability = timePassed / UnityEngine.Random.value  ;
 		if(probability > spawnValue){
 			GameObject newAsteroid = Instantiate(Asteroids[(int)(UnityEngine.Random.value * 5)]) as GameObject;
-			newAsteroid.GetComponent<AsteroidScript>().Speed = UnityEngine.Random.value * 30;
+			newAsteroid.GetComponent<AsteroidScript>().Speed = UnityEngine.Random.value * maxAsteroidSpeed;
 			float xPos = UnityEngine.Random.value * 16.5f;
-			Vector2 pos = newAsteroid.transform.position;
+			Vector3 pos = newAsteroid.transform.position;
 			pos.x = xPos > 8.25f ? -(xPos - 8.25f) : xPos;
+			pos.z = -5f;
 			newAsteroid.transform.position = pos;
 			liveAsteroids.Add(newAsteroid);
 			timePassed = 0f;
--- Unity/Assets/Scripts/SpaceshipScript.cs
+++ Unity/Assets/Scripts/SpaceshipScript.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class SpaceshipScript : MonoBehaviour {
 
+	public float speed = 5.0f;
+	public GameObject[] Lasers;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,29 @@ public class SpaceshipScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+ 		if(Input.GetKey(KeyCode.RightArrow) && transform.position.x < 8)
+		{
+ 			transform.position = transform.position + new Vector3(speed * Time.deltaTime,0,0);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -8)
+		{
+ 			transform.position = transform.position +  new Vector3(-speed * Time.deltaTime,0,0);
+		}
+		if(Input.GetKey(KeyCode.DownArrow) && transform.position.y > -5.5)
+		{
+			transform.position = transform.position +  new Vector3(0,-speed * Time.deltaTime,0);
+		}
+		if(Input.GetKey(KeyCode.UpArrow) && transform.position.y < 5.5)
+		{
+ 			transform.position = transform.position +  new Vector3(0,speed * Time.deltaTime,0);
+		}
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Instantiate(Lasers[0], transform.position + new Vector3(0, 1.25f, 0), Quaternion.identity);
+		}
 	}
+
+
+	 void OnTriggerEnter(Collider other) {
+        Debug.Log("test");
+    }
 }
